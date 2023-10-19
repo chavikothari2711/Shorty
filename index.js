426476b69976a7700f7aca53a8db06ca96f239d1
chavi kothari <kotharichavi1827@gmail.com>
2023-10-19 09:37:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ import connectDB from "./connectDB/db.js";
 import cors from "cors";
 import morgan from "morgan";
 import path from "path";
-
+import {fileURLToPath} from "url"
 connectDB();
 const app = express();
 const PORT=process.env.PORT || 8001;
+
+const filename = firlURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
