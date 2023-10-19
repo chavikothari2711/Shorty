18ceea971f3cc989af857a6ee3ff2da4f7449bef
chavi kothari <kotharichavi1827@gmail.com>
2023-10-19 09:48:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ connectDB();
 const app = express();
 const PORT=process.env.PORT || 8001;
 
-const filename = firlURLToPath(import.meta.url);
+const filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors());
